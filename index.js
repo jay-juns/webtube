e9fabd6af53be9fe9b32307ebc322385d43e53b6
jay-juns <necrozerg2@naver.com>
2020-05-17 18:21:02 +0900

    두번째 작업

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hi!');
+const express = require('express'); 
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http:///localhost:${PORT}`);
+}
+
+//request respond
+function handleHome(req, res) {
+    res.send('hello from home')
+}
+
+function handleProfile(req, res){
+    res.send("Your are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
